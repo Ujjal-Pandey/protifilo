0c45e9c0e374b5ddf43a9baf0ee8cdff9c54a6de
Ujjal Pandey <ujjalpandey11@gmail.com>
2026-05-17 12:50:45 +0545

    the data is updated

--- backendportfolio/server.js
+++ backendportfolio/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 
 dotenv.config();
 
